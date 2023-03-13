58751c2f5544098f0f73fa6c09fd45b06650f6bd
niramit555 <127605703+niramit555@users.noreply.github.com>
2023-03-13 17:12:21 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+        Secound se = new Secound();
+        System.out.println(se.getName());
+        System.out.println((days.satturday));
+    }
+}
